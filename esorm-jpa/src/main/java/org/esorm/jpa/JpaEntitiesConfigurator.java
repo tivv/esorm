363c38d976a43cbf2b3efd7b15886ca48c7f3f7d
Vitalii Tymchyshyn <tivv00@gmail.com>
2010-10-27 21:55:26 +0300

    A little forward on JPA parsing

--- esorm-jpa/src/main/java/org/esorm/jpa/JpaEntitiesConfigurator.java
+++ esorm-jpa/src/main/java/org/esorm/jpa/JpaEntitiesConfigurator.java
@@ -18,24 +18,25 @@
  */
 package org.esorm.jpa;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.lang.reflect.Modifier;
+import java.util.*;
 
-import javax.persistence.Entity;
-import javax.persistence.SecondaryTable;
+import javax.persistence.*;
 
 import org.esorm.EntitiesConfigurator;
 import org.esorm.LazyManagedEntityConfiguration;
 import org.esorm.entity.EntityProperty;
+import org.esorm.entity.db.Column;
 import org.esorm.entity.db.Table;
 import org.esorm.impl.*;
 import org.esorm.impl.db.ColumnImpl;
 import org.esorm.impl.db.TableImpl;
-import org.esorm.utils.PojoUtils;
+import org.esorm.utils.*;
 import org.esorm.utils.PojoUtils.NameFilter;
 import org.jcloudlet.bean.Property;
 import org.jcloudlet.bean.impl.PropertySelectorImpl;
 
+import static org.esorm.utils.StringUtils.*;
 /**
  * @author Vitalii Tymchyshyn
  *
@@ -43,11 +44,7 @@ import org.jcloudlet.bean.impl.PropertySelectorImpl;
 public class JpaEntitiesConfigurator
 implements EntitiesConfigurator
 {
-    /* (non-Javadoc)
-     * @see org.esorm.EntitiesConfigurator#resolveConfiguration(java.lang.String, java.lang.Iterable)
-     */
     /**
-     * @deprecated Use {@link #resolveConfiguration(String,Iterable<String>,boolean)} instead
      */
     public LazyManagedEntityConfiguration resolveConfiguration(String name,
                                                                Iterable<String> configurationLocations)
@@ -66,36 +63,94 @@ implements EntitiesConfigurator
             return null;
         LazyManagedEntityConfigurationImpl rc = new LazyManagedEntityConfigurationImpl(name);
         rc.setLocation(entityClass.getName());
-        Map<String, Table> tables = getTables(entityClass);
-        EntityProperty defaultIdProperty = null;
-        for (Property property: new PropertySelectorImpl(entityClass).select()) {
-            final EntityPropertyImpl entityProperty = new EntityPropertyImpl(property.name(), new ColumnImpl(table, property.name()) );
-            rc.getProperties().add(entityProperty); 
-            if ("id".equals(property.name())) {
-                defaultIdProperty = entityProperty;
-            }
+        Table primaryTable = getPrimaryTable(entityClass);
+        String primaryTableName = primaryTable.getName();
+        Map<String, List<PropertyBuilder>> properties = new HashMap<String, List<PropertyBuilder>>();
+        Map<String, PropertyBuilder> idProperties = fillProperties(entityClass,
+            primaryTable, properties);
+        
+        for (PropertyBuilder idProperty: idProperties.values()) {
+            rc.addIdColumn(idProperty.getColumn());
         }
-        if (rc.getIdColumns().isEmpty() && defaultIdProperty != null)
-            rc.addIdProperty(defaultIdProperty);
+        Map<String, Table> tables = new HashMap<String, Table>();
+        tables.put(primaryTableName, primaryTable);
+        addSecondaryTable(entityClass, tables, idProperties);
+        addSecondaryTables(entityClass, tables, idProperties);
         return rc;
     }
 
-    private Map<String, Table> getTables(Class<?> entityClass)
+    private Map<String, PropertyBuilder> fillProperties(Class<?> entityClass,
+                                          Table primaryTable,
+                                          Map<String, List<PropertyBuilder>> properties)
     {
-        Map<String, Table> rc = new HashMap<String, Table>();
+        PropertyBuilder defaultIdProperty = null;
+        Map<String, PropertyBuilder> idProperties = null;
+        for (Property property: new PropertySelectorImpl(entityClass).ignore(Transient.class).select()) {
+            if (property.field() != null && Modifier.isTransient(property.field().getModifiers()))
+                continue;
+            PropertyBuilder propertyBuilder = new PropertyBuilder(property, primaryTable);
+            String propertyTable = propertyBuilder.getTableName();
+            List<PropertyBuilder> tableProperties = properties.get(propertyTable);
+            if (tableProperties == null) {
+                tableProperties = new ArrayList<PropertyBuilder>();
+                properties.put(propertyTable, tableProperties);
+            }
+            tableProperties.add(propertyBuilder);
+            Id id = property.annotation(Id.class);
+            if (id != null) {
+                if (!primaryTable.getName().equals(propertyTable))
+                    throw new IllegalStateException("@Id annotation on " + entityClass + " property " + property.name() 
+                        + " assigned to table " + propertyTable + " different from primary table " + primaryTable.getName());
+                if (idProperties == null) {
+                    idProperties = new LinkedHashMap<String, PropertyBuilder>();
+                }
+                idProperties.put(propertyBuilder.getColumn().getName(), propertyBuilder);
+            } else if (idProperties == null && "id".equals(property.name()) && primaryTable.getName().equals(propertyTable)) {
+                defaultIdProperty = propertyBuilder;
+            }
+        }
+        if (idProperties == null && defaultIdProperty != null)
+            idProperties = Collections.singletonMap(defaultIdProperty.getColumn().getName(), defaultIdProperty);
+        return idProperties;
+    }
+
+    private void addSecondaryTables(Class<?> entityClass, Map<String, Table> rc, Map<String, PropertyBuilder> idProperties)
+    {
+        SecondaryTables secondaryTables = entityClass.getAnnotation(SecondaryTables.class);
+        if (secondaryTables != null) {
+            for (SecondaryTable secondaryTable : secondaryTables.value()) {
+                addSecondaryTable(rc, secondaryTable, idProperties);
+            }
+        }
+    }
+
+    private void addSecondaryTable(Class<?> entityClass, Map<String, Table> rc, Map<String, PropertyBuilder> idProperties)
+    {
+        SecondaryTable secondaryTable = entityClass.getAnnotation(SecondaryTable.class);
+        if (secondaryTable != null) {
+            addSecondaryTable(rc, secondaryTable, idProperties);
+        }
+    }
+
+    private Table getPrimaryTable(Class<?> entityClass)
+    {
         javax.persistence.Table primaryTable = 
             entityClass.getAnnotation(javax.persistence.Table.class);
         if (primaryTable != null) {
-            
+            final String tableName = notEmpty(primaryTable.name(), entityClass.getSimpleName());
+            return new TableImpl(
+                notEmpty(primaryTable.catalog()), notEmpty(primaryTable.schema()), tableName);
         } else {
-            rc.put(entityClass.getSimpleName(), new TableImpl(entityClass.getSimpleName()));
+            return new TableImpl(entityClass.getSimpleName());
         }
-        SecondaryTable secondaryTable = entityClass.getAnnotation(SecondaryTable.class);
-        if (secondaryTable != null) {
-            //TODO            
-        }
-            
-        return rc;
+    }
+
+    private void addSecondaryTable(Map<String, Table> rc,
+                                   SecondaryTable secondaryTable, Map<String, PropertyBuilder> primaryIdColumns)
+    {
+        final TableImpl table = new TableImpl(
+            notEmpty(secondaryTable.catalog()), notEmpty(secondaryTable.schema()), secondaryTable.name());
+        rc.put(secondaryTable.name(), table);
     }
 
     private static NameFilter NAME_FILTER = new NameFilter()
@@ -109,9 +164,77 @@ implements EntitiesConfigurator
                 return false;
             if (locationOverride)
                 return true;
-            if ("".equals(entityAnn.name()))
-                return clazz.getSimpleName().equals(name);
-            return entityAnn.name().equals(name);
+            return notEmpty(entityAnn.name(), clazz.getSimpleName()).equals(name);
         }
     };
+    
+    private static class PropertyBuilder {
+        private final Property property;
+        private final String tableName;
+        private final javax.persistence.Column columnAnn;
+        private Table table;
+        private ColumnImpl column;
+        private EntityPropertyImpl entityProperty;
+        
+        private PropertyBuilder(Property property, Table primaryTable) {
+            this.property = property;
+            this.columnAnn = property.annotation(javax.persistence.Column.class);
+            if (columnAnn != null) {
+                tableName = notEmpty(columnAnn.table(), primaryTable.getName());
+                if (primaryTable.getName().equals(tableName))
+                    setTable(primaryTable);
+            } else {
+                tableName = primaryTable.getName();
+                setTable(primaryTable);
+            }
+            
+        }
+
+        public Property getProperty()
+        {
+            return property;
+        }
+
+        public String getTableName()
+        {
+            return tableName;
+        }
+
+        public javax.persistence.Column getColumnAnn()
+        {
+            return columnAnn;
+        }
+
+        public Table getTable()
+        {
+            return table;
+        }
+
+        public void setTable(Table table)
+        {
+            assert this.table == null;
+            this.table = table;
+            column = new ColumnImpl(table, property.name());
+            if (columnAnn != null) {
+                if (columnAnn.name().length()>0)
+                    column.setName(columnAnn.name());
+                column.setInsertable(columnAnn.insertable());
+                column.setUpdateable(columnAnn.updatable());
+            }
+            entityProperty = new EntityPropertyImpl(property.name(), column);
+        }
+
+        public Column getColumn()
+        {
+            assert column != null;
+            return column;
+        }
+
+        public EntityPropertyImpl getEntityProperty()
+        {
+            assert entityProperty != null;
+            return entityProperty;
+        }
+
+    }
 }
