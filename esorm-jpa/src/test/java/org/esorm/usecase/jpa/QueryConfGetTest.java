68a8c011d830a38ff216929eb2648d78adb418a8
Vitalii Tymchyshyn <tivv00@gmail.com>
2010-11-01 22:22:32 +0200

    Add JPA test

---
+++ esorm-jpa/src/test/java/org/esorm/usecase/jpa/QueryConfGetTest.java
@@ -0,0 +1,54 @@
+/**
+ * 
+ * Copyright 2010 Vitalii Tymchyshyn
+ * This file is part of EsORM.
+ *
+ * EsORM is free software: you can redistribute it and/or modify
+ * it under the terms of the Lesser GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * EsORM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with EsORM.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.esorm.usecase.jpa;
+
+import javax.sql.DataSource;
+
+import static org.esorm.EsormUtils.*;
+import org.esorm.QueryConf;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.unitils.UnitilsJUnit4TestClassRunner;
+import org.unitils.database.annotations.TestDataSource;
+import org.unitils.dbunit.annotation.DataSet;
+
+/**
+ * @author Vitalii Tymchyshyn
+ *
+ */
+@RunWith(UnitilsJUnit4TestClassRunner.class)
+@DataSet
+public class QueryConfGetTest
+{
+    @TestDataSource
+    DataSource dataSource;
+    
+    
+    @Test
+    public void testGet() {
+        QueryConf conf = new QueryConf()
+            .connectionProvider(connect(dataSource));
+        EasyTableJpa res = conf.get(conf.getConfiguration(
+            EasyTableJpa.class), 1L);
+        Assert.assertEquals(1l, res.getId());
+        Assert.assertEquals("test", res.getName());
+    }
+     
+}
---
+++ esorm-jpa/src/test/resources/sql/def/0001_easy_table.sql
@@ -0,0 +1,4 @@
+create table EasyTable (
+ identifier bigint primary key,
+ name varchar(100) not null
+);
