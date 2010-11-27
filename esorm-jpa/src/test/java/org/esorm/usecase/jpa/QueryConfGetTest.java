57578b347233bc64765ca91532964e7aac21b8ad
Vitalii Tymchyshyn <tivv@acer.site>
2010-11-27 23:21:38 +0200

    Add one to one test

--- esorm-jpa/src/test/java/org/esorm/usecase/jpa/QueryConfGetTest.java
+++ esorm-jpa/src/test/java/org/esorm/usecase/jpa/QueryConfGetTest.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  * Copyright 2010 Vitalii Tymchyshyn
  * This file is part of EsORM.
  *
@@ -18,9 +18,6 @@
  */
 package org.esorm.usecase.jpa;
 
-import javax.sql.DataSource;
-
-import static org.esorm.EsormUtils.*;
 import org.esorm.QueryConf;
 import org.junit.Assert;
 import org.junit.Test;
@@ -29,26 +26,36 @@ import org.unitils.UnitilsJUnit4TestClassRunner;
 import org.unitils.database.annotations.TestDataSource;
 import org.unitils.dbunit.annotation.DataSet;
 
+import javax.sql.DataSource;
+
+import static org.esorm.EsormUtils.connect;
+
 /**
  * @author Vitalii Tymchyshyn
- *
  */
 @RunWith(UnitilsJUnit4TestClassRunner.class)
 @DataSet
-public class QueryConfGetTest
-{
+public class QueryConfGetTest {
     @TestDataSource
     DataSource dataSource;
-    
-    
+
+
     @Test
     public void testGet() {
         QueryConf conf = new QueryConf()
-            .connectionProvider(connect(dataSource));
+                .connectionProvider(connect(dataSource));
         EasyTableJpa res = conf.get(conf.getConfiguration(
-            EasyTableJpa.class), 1L);
+                EasyTableJpa.class), 1L);
         Assert.assertEquals(1l, res.getId());
         Assert.assertEquals("test", res.getName());
     }
-     
+
+    @Test
+    public void testGet_OneToOne() {
+        QueryConf conf = new QueryConf()
+                .connectionProvider(connect(dataSource));
+        OneToOneJpa res = conf.get(OneToOneJpa.class, 2L);
+        Assert.assertEquals(2l, res.getKey());
+        Assert.assertEquals(1l, res.getRelated().getId());
+    }
 }
--- esorm-jpa/src/test/resources/sql/def/0001_easy_table.sql
+++ esorm-jpa/src/test/resources/sql/def/0001_easy_table.sql
@@ -2,3 +2,8 @@ create table EasyTable (
  identifier bigint primary key,
  name varchar(100) not null
 );
+
+create table OneToOneJpa (
+ key bigint primary key,
+ easy_id bigint 
+);
